98c6c87f07e83128a6040adcedc237d0873ff940
Iván Legrán <ivanlegranbizarro@gmail.com>
2022-10-13 12:41:01 +0200

    Final entrega 1.5

--- entrega1.5.js
+++ entrega1.5.js
@@ -77,3 +77,39 @@ const llistar = function () {
 };
 
 llistar();
+
+/*
+Crea una funció que creï dos fitxers codificats en hexadecimal i en base64 respectivament, a partir del fitxer del nivell 1.
+*/
+
+const codificaFitxer = () => {
+  const fitxer = fs.readFileSync("frase.txt");
+  const base64 = fitxer.toString("base64");
+  const hex = fitxer.toString("hex");
+  fs.writeFileSync("frase.txt.base64", base64);
+  fs.writeFileSync("frase.txt.hex", hex);
+};
+
+codificaFitxer();
+
+/*
+Crea una funció que guardi els fitxers del punt anterior, ara encriptats amb l'algoritme aes-192-cbc, i esborri els fitxers inicials.
+*/
+
+const crypto = require("crypto");
+const algorithm = "aes-192-cbc";
+const key_in_bytes = crypto.randomBytes(24);
+
+const encriptarFitxer = () => {
+  const fitxer = fs.readFileSync("frase.txt");
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv(algorithm, key_in_bytes, iv);
+  let encrypted = cipher.update(fitxer, "utf8", "hex");
+  encrypted += cipher.final("hex");
+  fs.writeFileSync("frase.txt.aes", encrypted);
+  fs.unlinkSync("frase.txt");
+  fs.unlinkSync("frase.txt.base64");
+  fs.unlinkSync("frase.txt.hex");
+};
+
+encriptarFitxer();
